f850cb95abe3627d2f2ceed78b95552a7a2cdf1c
Espinoza <vitorespinoza18@gmail.com>
2023-01-25 20:03:19 -0300

    feat(account-controller): Começando rota de registro de conta

--- Controllers/AccountController.js
+++ Controllers/AccountController.js
@@ -7,3 +7,37 @@ const Account = require("../Models/Account");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+app.post("/account", async (request, response) => {
+  var { email, userName, password } = request.body;
+
+ let account = await Account.findOne({
+    where: {
+      email: email,
+    },
+  })
+
+    if(account) {
+        return response.status(200).json({
+            message: "Email already existing."
+        });
+      }
+      else {
+        Account.Create({
+            email: email,
+            userName: userName,
+            password: password,
+            slug: slugify(userName),
+          })
+            .then(() => {
+                return response.status(200).json({
+                    message: "Register account successfully."
+                });
+            })
+            .catch(() => {});
+      }
+  });
+
+
+  
+
